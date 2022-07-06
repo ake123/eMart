37c2dc4135b34dce9a080f290e93fd66e1babfcd
ake123 <akeketema@yahoo.com>
2022-07-06 12:08:31 +0300

    commit

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 
 const Products = () => {
@@ -81,21 +82,23 @@ const Products = () => {
           return (
             <>
               <div className="col-md-3 mb-4">
-                <div class="card h-100 text-center p-4" key={product.id}>
+                <div className="card h-100 text-center p-4" key={product.id}>
                   <img
                     src={product.image}
-                    class="card-img-top"
+                    className="card-img-top"
                     alt={product.title}
                     height="250px"
                   />
-                  <div class="card-body">
-                    <h5 class="card-title mb-0">
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">
                       {product.title.substring(0, 12)}...
                     </h5>
-                    <p class="card-text lead fw-bold">${product.price}</p>
-                    <a href="#" class="btn btn-outline-dark">
+                    <p className="card-text lead fw-bold">${product.price}</p>
+                    <NavLink
+                      to={`/products/${product.id}`}
+                      className="btn btn-outline-dark">
                       Buy Now
-                    </a>
+                    </NavLink>
                   </div>
                 </div>
               </div>
